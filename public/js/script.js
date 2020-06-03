813284a5110bc62b1637a1cb113629186ef7c8a2
rohitsraj12 <rohitsraj12@gmail.com>
2020-06-03 18:24:04 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
   $("#hamberger").click(function () {
-    // $(".header__nav").slideToggle();
     $(this).toggleClass("is__active");
     $("#header-nav").slideToggle(400);
-    // $("#header-social").slideToggle(400);
   });
 });
