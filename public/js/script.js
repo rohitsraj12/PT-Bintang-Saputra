ea0503994f4d3ddb9532a465b0ce02ced47cc564
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-06-05 20:22:34 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
+
   $("#hamberger").click(function () {
     $(this).toggleClass("is__active");
     $("#header-nav").slideToggle(400);
   });
+
+  $(".tab__list").click(function(event){
+    var attribute = $(this).find("a").attr("href");
+    event.preventDefault();
+    $(this).parent().find("li").removeClass("tab-active");
+    $(this).addClass("tab-active");
+    
+    $(".course-details-content").children().hide()
+    $(attribute).fadeIn();
+
+  })
+  
 });
